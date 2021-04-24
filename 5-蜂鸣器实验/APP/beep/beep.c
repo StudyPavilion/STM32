0b04fc38a29cf5a6a3c34b7c8792b0d2e1d89da0
StudyPavilion <1475109413@qq.com>
2021-04-24 12:24:26 +0800

    5.2-完成蜂鸣器实验（补充注释修改readme.txt文件）

--- 5-蜂鸣器实验/APP/beep/beep.c
+++ 5-蜂鸣器实验/APP/beep/beep.c
@@ -3,7 +3,7 @@
 void BEEP_Init(void)
 {
 	GPIO_InitTypeDef GPIO_Initstructure;
-	RCC_APB2PeriphClockCmd(BEEP_PORT_RCC, ENABLE);
+	RCC_APB2PeriphClockCmd(BEEP_PORT_RCC, ENABLE);//开启时钟
 	
 	GPIO_Initstructure.GPIO_Pin=BEEP_PIN;
 	GPIO_Initstructure.GPIO_Mode=GPIO_Mode_Out_PP;
@@ -11,4 +11,5 @@ void BEEP_Init(void)
 	GPIO_Init(BEEP_PORT, &GPIO_Initstructure);
 	
 	GPIO_ResetBits(BEEP_PORT, BEEP_PIN);
+	//蜂鸣器默认关闭，将端口置0
 }
--- 5-蜂鸣器实验/User/main.c
+++ 5-蜂鸣器实验/User/main.c
@@ -12,17 +12,18 @@ void delay(u32 i)//延时函数
 int main()
 {
 	SysTick_Init(72);
-	LED_Init();//初始化LED
-	BEEP_Init();
+	LED_Init();						//初始化LED
+	BEEP_Init();					//初始化蜂鸣器
 
 	
 	while(1)
 	{
-		LED0=!LED0;
-		BEEP=1;
-		delay_ms(400);
-		BEEP=0;
-		delay_ms(1600);
+		LED0=!LED0;					//DS0初始为低电平
+		BEEP=1;							//蜂鸣器高电平有效
+		delay_ms(400);			//精确延时400ms
+		BEEP=0;							//蜂鸣器置0
+		delay_ms(1600);			//精确延时1600ms
+
 	}
 	
 }
